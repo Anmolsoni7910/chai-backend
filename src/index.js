de16bbd67a5a5956c27f91e09e1e9738a4a4050e
anmolsoni7910 <anmolsoni7910@gmail.com>
2024-04-06 05:18:56 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+/*
+// 1st approch
+import express from 'express';
+
+const app = express();
+
+;(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", () => {
+            console.log("ERRR: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on http://localhost:${process.env.PORT}`);
+        })
+
+    }catch(error){
+        console.error("ERROR: ",error);
+        throw error;
+    }
+})()   //iff
+*/
