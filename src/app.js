233ce25b262d6baa947105d9206e9a7d13bed3fd
anmolsoni7910 <anmolsoni7910@gmail.com>
2024-04-06 18:50:04 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+//routes import
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users", userRouter);  //router ko seprate kar deya hai to ab app.use karna parega router as a middleware aayga
+// yaha se control fhir userRouter ke pass jayga or baha se kes route pe jana hai yaha declare keya jata hai
+// jasse /users/register ya /users/login
+//matlab control sethe userRouter ke pass chala jayga
+
 export default app;
