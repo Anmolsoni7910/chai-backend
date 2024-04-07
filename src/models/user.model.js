4cf3f516198af5946920c893cf9e3dbec5a61723
anmolsoni7910 <anmolsoni7910@gmail.com>
2024-04-07 08:34:22 +0530

    user Controller basic complete

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,10 +54,11 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next(); 
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
+//userSchema.methods. -> custom methods ke leya lekha jata hai
 userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password,this.password);    //return true or false
 }
